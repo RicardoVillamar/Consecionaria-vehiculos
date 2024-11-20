6e9247889bd422df5e6e62d51d60f79232df4053
Ricardo <ricardo.villamarmin@ug.edu.ec>
2024-11-20 11:25:58 -0500

    update Compra Venta

--- Modelo/Cliente.cs
+++ Modelo/Cliente.cs
@@ -9,7 +9,7 @@ namespace Modelo
     public class Cliente
     {
 
-    //Se está tomando el valor de identificación del cliente que se proporciona al crear un nuevo cliente.
+        //Se está tomando el valor de identificación del cliente que se proporciona al crear un nuevo cliente.
 
         private string _idCliente;
         public string IdCliente
@@ -18,7 +18,7 @@ namespace Modelo
             set { _idCliente = value; }
         }
 
-   //Se está tomando el nombre del cliente que se proporcionó al crear un nuevo cliente
+        //Se está tomando el nombre del cliente que se proporcionó al crear un nuevo cliente
 
         private string _Nombre;
         public string Nombre
@@ -27,7 +27,7 @@ namespace Modelo
             set { _Nombre = value; }
         }
 
-   //Esta línea se encarga de asignar el valor del apellido que se proporcionó al crear un nuevo cliente
+        //Esta línea se encarga de asignar el valor del apellido que se proporcionó al crear un nuevo cliente
 
         private string _Apellido;
         public string Apellido
@@ -35,7 +35,7 @@ namespace Modelo
             get { return _Apellido; }
             set { _Apellido = value; }
 
-   //Esta línea se encarga de asignar el valor del cedula que se proporcionó al crear un nuevo cliente
+            //Esta línea se encarga de asignar el valor del cedula que se proporcionó al crear un nuevo cliente
 
         }
         private string _Cedula;
@@ -45,7 +45,7 @@ namespace Modelo
             set { _Cedula = value; }
         }
 
- //Esta línea se encarga de asignar el valor del telefono que se proporcionó al crear un nuevo cliente
+        //Esta línea se encarga de asignar el valor del telefono que se proporcionó al crear un nuevo cliente
 
         private int _Telefono;
         public int Telefono
@@ -53,7 +53,7 @@ namespace Modelo
             get { return _Telefono; }
             set { _Telefono = value; }
         }
-//Esta línea se encarga de asignar el valor del Email que se proporcionó al crear un nuevo cliente
+        //Esta línea se encarga de asignar el valor del Email que se proporcionó al crear un nuevo cliente
 
         private string _Email;
         public string Email
@@ -62,3 +62,4 @@ namespace Modelo
             set { _Email = value; }
         }
     }
+}
--- Modelo/CompraVenta.cs
+++ Modelo/CompraVenta.cs
@@ -8,9 +8,11 @@ namespace Modelo
 {
     public class CompraVenta
     {
-        
-// Estas propiedades parecen almacenar información relevante sobre una compra o venta, como el cliente involucrado, los detalles de los productos comprados, el tipo de operación, la fecha, el subtotal y el total.
+        /* Estas propiedades parecen almacenar información relevante sobre una compra o venta, 
+        como el cliente involucrado, los detalles de los productos comprados, el tipo de operación,
+        la fecha, el subtotal y el total.*/
 
+        // Representa al cliente asociado con la compra o venta
         private Cliente _cliente;
         public Cliente Cliente
         {
@@ -19,49 +21,46 @@ namespace Modelo
         }
 
         //Representan los productos o servicios que se están comprando o vendiendo.
-
-        private List<Cliente> _clienteList;
+        private List<Detalle> _detalles;
         public List<Detalle> Detalles
         {
-            get { return; _clienteList }
-            set { _clienteList = value; }
+            get { return _detalles; }
+            set { _detalles = value; }
         }
 
-    //Indica el tipo de operación que se está realizando, es decir, si es una compra o una venta.
-
-            private TipoCV;
-            public string _TipoCV {
-
-            get { return; _TipoCV }
-            set {_TipoCV = value; }
-    }
-        
-     //Es redundante y puede eliminarse.
-
-        private _fecha;
-            public DateTime _fecha
+        //Indica el tipo de operación que se está realizando, es decir, si es una compra o una venta.
+        private string _tipoCV;
+        public string TipoCV
         {
+            get { return _tipoCV; }
+            set { _tipoCV = value; }
+        }
 
-            get { return; _fecha }
+        //Es redundante y puede eliminarse.
+        private DateTime _fecha;
+        public DateTime Fecha
+        {
+            get { return _fecha; }
             set { _fecha = value; }
         }
 
-      //Este método debería contener la lógica necesaria para realizar estos cálculos basados en los detalles de la compra.
-
-        private _subtotal;
-            public decimal _subtotal
+        //Este método debería contener la lógica necesaria para realizar estos cálculos basados en los detalles de la compra.
+        private decimal _subtotal;
+        public decimal Subtotal
         {
-
-            get { return; _subtotal }
+            get { return _subtotal; }
             set { _subtotal = value; }
         }
-        private _total;
-            public decimal _total
-        {
 
-            get { return; _total }
+        // Total calculado
+        private decimal _total;
+        public decimal Total
+        {
+            get { return _total; }
             set { _total = value; }
         }
-    }
 
     }
+
+
+}
